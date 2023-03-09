f774a86105221261724c453f15948fc81b6d7be3
Jonas Holbech <jh@jonasholbech.dk>
2023-03-09 07:46:25 +0100

    starter template

--- sketch.js
+++ sketch.js
@@ -1,58 +1,40 @@
-const el = document.querySelector("#sketch");
-el.src = el.src + "?" + Date.now();
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(20);
+  //Fullscreen
+  //createCanvas(windowWidth, windowHeight);
+
+  //Inside a parent element
+  const canvas = createCanvas(700, 700);
+  canvas.parent("sketch-holder");
+
+  //basic setup
+  frameRate(40);
   background(0, 0, 0);
-  fill("hotpink");
-  square(100, 100, 50);
   fill("lightblue");
 }
-let x = 0;
-let frames = 0;
+
+//A helper, getRandInt(10,0) gives us a random number between 10 and 0
 function getRandInt(max, min = 0) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
-console.log(getRandInt(0, 10));
-console.log(getRandInt(10, 0));
-function getRandColor() {
-  return [getRandInt(255), getRandInt(255), getRandInt(255)];
-}
-function rX() {
-  return getRandInt(0, width);
-}
-function rY() {
-  return getRandInt(0, height);
-}
+
 function draw() {
-  clear();
-  fill("black");
-  textSize(32);
-  text("X: " + accelerationX.toFixed(3), 10, 30);
-  text("Y: " + accelerationY.toFixed(3), 10, 100);
-  text("Z: " + accelerationZ.toFixed(3), 10, 200);
-  /* frames++;
-  let shape = getRandInt(2);
-  shape = 0;
-  fill(...getRandColor());
-  if (shape === 0) {
-    square(rX(), rY(), getRandInt(10, 50));
-  } else if (shape === 1) {
-    circle(rX(), rY(), getRandInt(10, 50));
-  } else {
-    const r1 = rX();
-    const y1 = rY();
-    const r2 = r1 + getRandInt(25);
-    const y2 = y1 + getRandInt(25, 0);
-    const r3 = r1 + getRandInt(0, -25);
-    const y3 = y1 + getRandInt(0, -25);
-    triangle(r1, y1, r2, y2, r3, y3);
-  }
-  if (frames > 100) {
-    noLoop();
-  } */
+  //clear();
 }
 function mouseDragged() {
-  fill(getRandInt(255), getRandInt(255), getRandInt(255));
-  square(mouseX, mouseY, 50);
+  //mouseX
+  //mouseY
+}
+function mouseMoved() {
+  //mouseX
+  //mouseY
+}
+function mouseClicked() {
+  //mouseX
+  //mouseY
+}
+
+function keyPressed() {
+  if (keyIsDown(UP_ARROW)) {
+    //
+  }
 }
